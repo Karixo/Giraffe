3e5d13dda6609157a48e139688a4547eff7433c6
Karixo <karixo717@gmail.com>
2021-11-12 12:12:08 +0100

    Commit 2 return statement

--- 13. Return Statement.py
+++ 13. Return Statement.py
@@ -1,1 +1,8 @@
 
+def cube(num):
+    return num*num*num  # return zwróci wartość
+    print("code")  # nie wyświetli się bo return kończy funkcję
+
+
+result = cube(2)  # przypisanie zmiennej return wyniku funkcji cube
+print(result)
