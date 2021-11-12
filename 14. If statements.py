b8294f5cedd0c256c42e407d2ecb2faa616f3788
Karixo <karixo717@gmail.com>
2021-11-12 13:18:24 +0100

    Commit 4

--- 14. If statements.py
+++ 14. If statements.py
@@ -1,7 +1,17 @@
 is_male = False
-is_tall = True
+is_tall = False
 
-if is_male:  # jeśli warunek jest true, kod sie wykona
-    print("You are a male")
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male!")
+elif not(is_male) and is_tall:
+    print("You are a not a male but you are tall")
 else:  # jeśl nie, wykona się to
-    print("You are not a male")
+    print("You are neither a male or tall")
+
+# if is_male:  # jeśli warunek jest true, kod sie wykona
+# if\or\and
+# else
+# elif - else if - taki pythonowy skrót
+# not(zmienna) - if false
