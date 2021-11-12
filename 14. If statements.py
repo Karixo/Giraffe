4bdd97c0ef2fcbcaba2a3ce9172ecad20c49b4ae
Karixo <karixo717@gmail.com>
2021-11-12 13:06:51 +0100

    Commit 3

---
+++ 14. If statements.py
@@ -0,0 +1,7 @@
+is_male = False
+is_tall = True
+
+if is_male:  # jeśli warunek jest true, kod sie wykona
+    print("You are a male")
+else:  # jeśl nie, wykona się to
+    print("You are not a male")
