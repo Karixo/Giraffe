0e3dae169853ac36aefe7907c85e2941d92de749
Karixo <karixo717@gmail.com>
2021-11-13 16:42:41 +0100

    Commit 7

---
+++ 17. Dictionaries.py
@@ -0,0 +1,12 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    0: "Zero",
+}
+
+print(monthConversions.get(0, "Not a valid key"))
+
+
+# print(monthConversions["Feb"]) - zwraca February
+# print(monthConversions.get("Feb") - zwraca February
