04b1c6c26eb7d48b168cb010fa792181321a5dfa
Karixo <karixo717@gmail.com>
2021-11-13 18:26:58 +0100

    Commit 9 moduły

---
+++ 28. Modules and Pip.py
@@ -0,0 +1,20 @@
+"""
+moduły to w sumie importowanie
+import plik
+po zaimportowaniu mogę sobie zrobić np
+print(plik. ...) i wyświetli mi zmienne i funkcje z tamtego pliku które moge sobie następnie użyć w
+aktualnym kodzie
+*Python Module Index - strona z modułami to zaimportowania
+
+"""
+#pip
+"""
+pip to package manager, pomaga instalować moduły spoza 
+standardowej biblioteki modułów pythona
+pip jest instalowany razem z pythonem
+ale uzywa się go poprzez terminal
+najlepiej w jako admin(bo mi wywaliło bład w instalacji)
+prosta komenda pip:
+pip install nazwa_modułu_który_ściągasz
+i się instaluje elegancko
+"""
